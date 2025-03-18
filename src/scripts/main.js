d55b6ba50520b094de8be8d6de8c80335c28fe0f
Artemiy23 <a.utkin-8@mail.ru>
2025-03-18 16:30:10 +0700

    new file:   index.html 	new file:   shop.html 	new file:   src/fonts/Roboto-Medium.woff2 	new file:   src/fonts/Roboto-Regular.woff2 	new file:   src/imgs/card-flower-img-1.png 	new file:   src/imgs/card-flower-img-2.png 	new file:   src/imgs/card-flower-img-3.png 	new file:   src/imgs/card-flower-img-4.png 	new file:   src/imgs/comment-user-mini-img-1.png 	new file:   src/imgs/comment-user-mini-img-2.png 	new file:   src/imgs/comment-user-mini-img-3.png 	new file:   src/imgs/grid-flower-img-1.png 	new file:   src/imgs/grid-flower-img-2.png 	new file:   src/imgs/grid-flower-img-3.png 	new file:   src/imgs/grid-flower-img-4.png 	new file:   src/imgs/grid-flower-img-5.png 	new file:   src/imgs/grid-flower-img-6.png 	new file:   src/imgs/icons/active-star-icon.svg 	new file:   src/imgs/icons/at-icon.svg 	new file:   src/imgs/icons/cart-shopping-mini-icon.svg 	new file:   src/imgs/icons/cart-shopping-solid.svg 	new file:   src/imgs/icons/eye-icon.svg 	new file:   src/imgs/icons/filter-icon.svg 	new file:   src/imgs/icons/heart-icon.svg 	new file:   src/imgs/icons/inactive-star-icon.svg 	new file:   src/imgs/icons/magnifying-glass-icon.svg 	new file:   src/imgs/icons/map-label-icon.svg 	new file:   src/imgs/icons/phone-icon.svg 	new file:   src/imgs/icons/price-filter-icon.svg 	new file:   src/imgs/icons/user-solid.svg 	new file:   src/imgs/post-img-1.png 	new file:   src/imgs/post-img-2.png 	new file:   src/imgs/post-img-3.png 	new file:   src/imgs/post-user-mini-img.png 	new file:   src/scripts/main.js 	new file:   src/styles/main.css 	new file:   src/styles/shop.css

---
+++ src/scripts/main.js
@@ -0,0 +1,43 @@
+const burgerMenu = document.querySelector('#burger-menu');
+const burgerBtn = document.querySelector('#burger-menu-btn');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
+let lastScroll = 0;
+let delta = 0;
+
+burgerBtn.onclick = function () {
+    burgerBtn.classList.toggle('active');
+    document.querySelector('body').classList.toggle('scroll-block');
+    if (burgerMenu.classList.toggle('open')) {
+        burgerMenu.classList.remove('close');
+        burgerMenu.removeAttribute('hidden');
+    } else {
+        burgerMenu.classList.add('close');
+    }
+}
+
+burgerMenu.addEventListener('animationend', function (e) {
+    if (e.animationName == 'menu-close') {
+        e.target.hidden = true;
+    }
+})
+
+window.addEventListener('scroll', function (e) {
+    const currentScroll = this.window.scrollY;
+    
+    console.log(delta);
+    console.log(currentScroll + ' current');
+    console.log(lastScroll + ' last');
+    if (currentScroll <= 100) {
+        body.classList.remove('scroll-down');
+        body.classList.add('scroll-up');
+        return;
+    } else if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up');
+        body.classList.add('scroll-down');
+    } else if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.add('scroll-up');
+        body.classList.remove('scroll-down');
+    }
+    lastScroll = currentScroll;
+})
